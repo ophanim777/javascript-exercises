6f6abd7433d4088ce7ccfe487e0d15433a8cbbc0
ophanim777 <rikyjo49@gmail.com>
2025-06-18 09:30:32 +0700

    edit greeting

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -15,7 +15,7 @@ const thisYear = 1965;
 const birthYear = 1947;
 const fullName = firstName + " " + lastName;
 const age = thisYear - birthYear;
-const greeting ="Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear-birthYear) + " years old."
+const greeting ="Hello! My name is " + fullName + " and I am " + age + " years old."
 
 
 // Do not change this
