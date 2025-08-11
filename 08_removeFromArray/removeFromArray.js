42f8d18516af219a4ca98afb9814b4e9fe2dfb19
ophanim777 <rikyjo49@gmail.com>
2025-08-11 10:39:58 +0700

    create variables

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,6 +1,5 @@
-removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
-const removeFromArray = function() {
-};
-
+const removeFromArray = function (array, ...args) {
+    
+}
 // Do not edit below this line
 module.exports = removeFromArray;
