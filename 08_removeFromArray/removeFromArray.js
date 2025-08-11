f69b8c4b6f27d14596aec189ff0f89c4421e49ba
ophanim777 <rikyjo49@gmail.com>
2025-08-11 10:45:24 +0700

    add for each

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
 const removeFromArray = function (array, ...args) {
     const newArray = [];
-}
+    aarray.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  })
+};
 // Do not edit below this line
 module.exports = removeFromArray;
