8a969c555a923707010d5877bbe3093517380064
ophanim777 <rikyjo49@gmail.com>
2025-08-18 10:51:27 +0700

    add limit

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,7 +1,8 @@
 let min = 1;
 let max = 4;
 const sumAll = function(min, max) {
-
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
 };
 
 // Do not edit below this line
