d16dccee0023515d527e081afdcfda92ef3ce220
ophanim777 <rikyjo49@gmail.com>
2025-08-18 10:56:24 +0700

    add array

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -3,6 +3,7 @@ let max = 4;
 const sumAll = function(min, max) {
   if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
   if (min < 0 || max < 0) return "ERROR";
+  if (min > max) [min, max] = [max, min];
 };
 
 // Do not edit below this line
