efe38f630068e4fe2ff9c1706c69082a12a8b526
ophanim777 <rikyjo49@gmail.com>
2025-08-18 11:10:35 +0700

    add const

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,9 @@
 let year = ''
 const leapYears = function(year) {
+    const divideByFour = year % 4 === 0;
+    const century = year % 100 === 0;
+    const divideByFourHundred = year % 400 === 0;
+
 
 };
 
